a1cd95784edbab391a3ceb11a957ba9812a159cf
Abid Omar <contact@omarabid.com>
2021-12-20 14:17:53 +0100

    refactor(utils): move utility functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,2 @@
 pub mod pyroscope;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod utils;
--- src/pyroscope.rs
+++ src/pyroscope.rs
@@ -36,11 +36,12 @@ use std::collections::HashMap;
 
 use pprof::ProfilerGuardBuilder;
 use pprof::Result;
-use pprof::report::Report;
 
 use tokio::sync::mpsc;
 
 use libc::c_int;
+use crate::utils::pyroscope_ingest;
+use crate::utils::merge_tags_with_app_name;
 
 pub struct PyroscopeAgentBuilder {
     inner_builder: ProfilerGuardBuilder,
@@ -148,93 +149,5 @@ impl PyroscopeAgent {
         self.handler = Some(handler);
 
         Ok(())
-    }
-}
-
-async fn pyroscope_ingest<S: AsRef<str>, N: AsRef<str>>(
-            report: Report,
-            url: S,
-            application_name: N,
-        ) -> Result<()> {
-            let mut buffer = Vec::new();
-
-            report.fold(true, &mut buffer)?;
-
-            if buffer.is_empty() {
-                return Ok(());
-            }
-
-            let client = reqwest::Client::new();
-            // TODO: handle the error of this request
-
-            let start: u64 = report 
-                .timing
-                .start_time
-                .duration_since(std::time::UNIX_EPOCH)
-                .unwrap()
-                .as_secs();
-            let s_start = start - start.checked_rem(10).unwrap();
-            // This assumes that the interval between start and until doesn't
-            // exceed 10s
-            let s_until = s_start + 10;
-
-            client
-                .post(format!("{}/ingest", url.as_ref()))
-                .header("Content-Type", "binary/octet-stream")
-                .query(&[
-                    ("name", application_name.as_ref()),
-                    ("from", &format!("{}", s_start)),
-                    ("until", &format!("{}", s_until)),
-                    ("format", "folded"),
-                    ("sampleRate", &format!("{}", report.sample_rate)),
-                    ("spyName", "pprof-rs"),
-                ])
-                .body(buffer)
-                .send()
-                .await?;
-
-            Ok(())
-        }
-
-fn merge_tags_with_app_name(application_name: String, tags: HashMap<String, String>) -> String {
-    let mut tags_vec = tags
-        .into_iter()
-        .filter(|(k, _)| k != "__name__")
-        .map(|(k, v)| format!("{}={}", k, v))
-        .collect::<Vec<String>>();
-    tags_vec.sort();
-    let tags_str = tags_vec.join(",");
-
-    if !tags_str.is_empty() {
-        format!("{}{{{}}}", application_name, tags_str,)
-    } else {
-        application_name
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::collections::HashMap;
-
-    use crate::pyroscope::merge_tags_with_app_name;
-
-    #[test]
-    fn merge_tags_with_app_name_with_tags() {
-        let mut tags = HashMap::new();
-        tags.insert("env".to_string(), "staging".to_string());
-        tags.insert("region".to_string(), "us-west-1".to_string());
-        tags.insert("__name__".to_string(), "reserved".to_string());
-        assert_eq!(
-            merge_tags_with_app_name("my.awesome.app.cpu".to_string(), tags),
-            "my.awesome.app.cpu{env=staging,region=us-west-1}".to_string()
-        )
-    }
-
-    #[test]
-    fn merge_tags_with_app_name_without_tags() {
-        assert_eq!(
-            merge_tags_with_app_name("my.awesome.app.cpu".to_string(), HashMap::default()),
-            "my.awesome.app.cpu".to_string()
-        )
     }
 }
