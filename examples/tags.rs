9d91976c93ade073540a17206abcdb4294f83b11
Abid Omar <contact@omarabid.com>
2021-12-22 19:38:36 +0100

    imp(examples): add tags example

---
+++ examples/tags.rs
@@ -0,0 +1,50 @@
+extern crate pyroscope;
+
+use pyroscope::{PyroscopeAgent, Result};
+
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 | 1 => 1,
+        n => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut agent = PyroscopeAgent::builder("http://localhost:4040", "fibonacci")
+        .frequency(100)
+        .tags(
+            [("Hostname".to_owned(), "pyroscope".to_owned())]
+                .iter()
+                .cloned()
+                .collect(),
+        )
+        .build()?;
+
+    agent.start()?;
+
+
+    agent.add_tags(
+        [("series".to_owned(), "Number 1".to_owned()), ("order".to_owned(), "first".to_owned())]
+            .iter()
+            .cloned()
+            .collect(),
+    )?;
+    let result = fibonacci(47);
+    println!("fibonacci {}", result);
+    agent.remove_tags(vec!["order".to_owned()])?;
+
+    agent.add_tags(
+        [("series".to_owned(), "Number 2".to_owned())]
+            .iter()
+            .cloned()
+            .collect(),
+    )?;
+    let result = fibonacci(47);
+    println!("fibonacci {}", result);
+    agent.remove_tags(vec!["series".to_owned()])?;
+
+    agent.stop().await?;
+
+    Ok(())
+}
