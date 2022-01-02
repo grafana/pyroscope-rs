01a292c4931c03a3aa39a9c499ee03b1404c43d5
Abid Omar <contact@omarabid.com>
2022-01-02 10:30:59 +0100

    refactor(config): add Config struct

--- src/pyroscope.rs
+++ src/pyroscope.rs
@@ -17,43 +17,30 @@ use crate::error::Result;
 //use crate::scheduler::{Event, PyroscopeScheduler};
 use crate::timer::Timer;
 
-pub struct PyroscopeAgentBuilder {
-    backend: Arc<Mutex<dyn Backend>>,
-
+#[derive(Clone, Debug)]
+pub struct PyroscopeConfig {
     url: String,
     application_name: String,
     tags: HashMap<String, String>,
     sample_rate: i32,
 }
 
-impl PyroscopeAgentBuilder {
+impl PyroscopeConfig {
     pub fn new<S: AsRef<str>>(url: S, application_name: S) -> Self {
         Self {
             url: url.as_ref().to_owned(),
             application_name: application_name.as_ref().to_owned(),
             tags: HashMap::new(),
-            backend: Arc::new(Mutex::new(Pprof::default())), // Default Backend
             sample_rate: 100i32,
         }
     }
 
-    pub fn backend<T: 'static>(self, backend: T) -> Self
-    where
-        T: Backend,
-    {
+    pub fn sample_rate(self, sample_rate: i32) -> Self {
         Self {
-            backend: Arc::new(Mutex::new(backend)),
+            sample_rate,
             ..self
         }
     }
-
-    pub fn frequency(self, frequency: i32) -> Self {
-        Self {
-            sample_rate: frequency,
-            ..self
-        }
-    }
-
     pub fn tags(self, tags: &[(&str, &str)]) -> Self {
         // Convert &[(&str, &str)] to HashMap(String, String)
         let tags_hashmap: HashMap<String, String> = tags
@@ -68,14 +55,71 @@ impl PyroscopeAgentBuilder {
             ..self
         }
     }
+}
+
+pub struct PyroscopeAgentBuilder {
+    backend: Arc<Mutex<dyn Backend>>,
+
+    //url: String,
+    //application_name: String,
+    //tags: HashMap<String, String>,
+    //sample_rate: i32,
+
+    config: PyroscopeConfig,
+}
+
+impl PyroscopeAgentBuilder {
+    pub fn new<S: AsRef<str>>(url: S, application_name: S) -> Self {
+        Self {
+            //url: url.as_ref().to_owned(),
+            //application_name: application_name.as_ref().to_owned(),
+            //tags: HashMap::new(),
+            backend: Arc::new(Mutex::new(Pprof::default())), // Default Backend
+            //sample_rate: 100i32,
+
+            config: PyroscopeConfig::new(url, application_name),
+        }
+    }
+
+    pub fn backend<T: 'static>(self, backend: T) -> Self
+    where
+        T: Backend,
+    {
+        Self {
+            backend: Arc::new(Mutex::new(backend)),
+            ..self
+        }
+    }
+
+    pub fn sample_rate(self, sample_rate: i32) -> Self {
+        Self {
+            config: self.config.sample_rate(sample_rate),
+            ..self
+        }
+    }
+
+    pub fn tags(self, tags: &[(&str, &str)]) -> Self {
+        // Convert &[(&str, &str)] to HashMap(String, String)
+        //let tags_hashmap: HashMap<String, String> = tags
+            //.to_owned()
+            //.iter()
+            //.cloned()
+            //.map(|(a, b)| (a.to_owned(), b.to_owned()))
+            //.collect();
+
+        Self {
+            config: self.config.tags(tags),
+            ..self
+        }
+    }
 
     pub fn build(self) -> Result<PyroscopeAgent> {
         // Initiliaze the backend
         let backend = Arc::clone(&self.backend);
-        backend.lock()?.initialize(self.sample_rate)?;
+        backend.lock()?.initialize(self.config.sample_rate)?;
 
         // Create Tags Arc<Mutex<>>
-        let tags = Arc::new(Mutex::new(self.tags));
+ //       let tags = Arc::new(Mutex::new(self.tags));
 
         // Initialize Scheduler
         //let scheduler = PyroscopeScheduler::initialize(
@@ -93,10 +137,11 @@ impl PyroscopeAgentBuilder {
         Ok(PyroscopeAgent {
             backend: self.backend,
             //scheduler,
-            url: self.url,
-            application_name: self.application_name,
-            tags: Arc::clone(&tags),
-            sample_rate: self.sample_rate,
+            //url: self.url,
+            //application_name: self.application_name,
+            //tags: Arc::clone(&tags),
+            //sample_rate: self.sample_rate,
+            config: self.config,
             timer,
             tx: None,
             handle: None,
@@ -105,6 +150,7 @@ impl PyroscopeAgentBuilder {
     }
 }
 
+#[derive(Debug)]
 pub struct PyroscopeAgent {
     pub backend: Arc<Mutex<dyn Backend>>,
     //scheduler: PyroscopeScheduler,
@@ -113,10 +159,12 @@ pub struct PyroscopeAgent {
     handle: Option<JoinHandle<()>>,
     running: Arc<(Mutex<bool>, Condvar)>,
 
-    url: String,
-    application_name: String,
-    tags: Arc<Mutex<HashMap<String, String>>>,
-    sample_rate: i32,
+    // Session Data
+    pub config: PyroscopeConfig,
+    //url: String,
+    //application_name: String,
+    //tags: Arc<Mutex<HashMap<String, String>>>,
+    //sample_rate: i32,
 }
 
 impl PyroscopeAgent {
@@ -149,6 +197,9 @@ impl PyroscopeAgent {
         let (lock, cvar) = &*pair;
         let mut running = lock.lock().unwrap();
         *running = true;
+        drop(lock);
+        drop(cvar);
+        drop(running);
 
         //self.scheduler.tx.send(Event::Start).unwrap();
         let (tx, rx): (Sender<u64>, Receiver<u64>) = channel();
@@ -183,7 +234,8 @@ impl PyroscopeAgent {
         // Wait for the Thread to finish
         let pair = Arc::clone(&self.running);
         let (lock, cvar) = &*pair;
-        cvar.wait_while(lock.lock().unwrap(), |running| {*running}).unwrap();
+        cvar.wait_while(lock.lock().unwrap(), |running| *running)
+            .unwrap();
 
         // Create a clone of Backend
         let backend = Arc::clone(&self.backend);
@@ -196,12 +248,13 @@ impl PyroscopeAgent {
         Ok(())
     }
 
+    
     pub fn add_tags(&mut self, tags: &[(&str, &str)]) -> Result<()> {
         // Stop Agent
-        self.stop()?;
+        //self.stop()?;
 
         // Restart Agent
-        self.start()?;
+        //self.start()?;
 
         // Convert &[(&str, &str)] to HashMap(String, String)
         let tags_hashmap: HashMap<String, String> = tags
@@ -211,34 +264,36 @@ impl PyroscopeAgent {
             .map(|(a, b)| (a.to_owned(), b.to_owned()))
             .collect();
 
+        self.config.tags.extend(tags_hashmap);
+
         // Create a clone of tags
-        let tags_arc = Arc::clone(&self.tags);
+        //let tags_arc = Arc::clone(&self.tags);
         // Extend tags with tags_hashmap
-        tags_arc.lock()?.extend(tags_hashmap);
+        //tags_arc.lock()?.extend(tags_hashmap);
 
         Ok(())
     }
 
     pub fn remove_tags(&mut self, tags: &[&str]) -> Result<()> {
         // Stop Agent
-        self.stop()?;
+        //self.stop()?;
 
         // Create a clone of tags
-        let tags_arc = Arc::clone(&self.tags);
+        //let tags_arc = Arc::clone(&self.tags);
         // Get a lock of tags
-        let mut tags_lock = tags_arc.lock()?;
+        //let mut tags_lock = tags_arc.lock()?;
 
         // Iterate through every tag
         tags.iter().for_each(|key| {
             // Remove tag
-            tags_lock.remove(key.to_owned());
+            self.config.tags.remove(key.to_owned());
         });
 
         // Drop lock
-        drop(tags_lock);
+        //drop(tags_lock);
 
         // Restart Agent
-        self.start()?;
+        //self.start()?;
 
         Ok(())
     }
