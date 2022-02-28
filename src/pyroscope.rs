c0a64b7b3d3b9a166bf3648ff3c53534a24d68b1
Bernhard Schuster <bernhard@ahoi.io>
2022-02-28 13:23:59 +0100

    remove another stray license comment

--- src/pyroscope.rs
+++ src/pyroscope.rs
@@ -7,9 +7,6 @@ use std::{
     thread::JoinHandle,
 };
 
-// Licensed under the Apache License, Version 2.0 <LICENSE or
-// https://www.apache.org/licenses/LICENSE-2.0>. This file may not be copied, modified, or distributed
-// except according to those terms.
 use crate::{
     backends::{pprof::Pprof, Backend},
     error::Result,
