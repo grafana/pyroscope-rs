66414f70c400adb17859964de0af0259f595ec5b
Abid Omar <contact@omarabid.com>
2022-02-28 22:02:50 +0100

    imp(cli): add app_config module

---
+++ pyroscope_cli/utils/src/app_config.rs
@@ -0,0 +1,94 @@
+use config::{Config, Environment};
+use lazy_static::lazy_static;
+use serde::Deserialize;
+use std::ops::Deref;
+use std::path::Path;
+use std::sync::RwLock;
+
+use super::error::Result;
+
+// CONFIG static variable. It's actually an AppConfig
+// inside an RwLock.
+lazy_static! {
+    static ref CONFIG: RwLock<Config> = RwLock::new(Config::new());
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub debug: bool,
+}
+
+impl AppConfig {
+    /// Initialize AppConfig.
+    pub fn init(default_config: Option<&str>) -> Result<()> {
+        let mut settings = Config::new();
+
+        // Embed file into executable
+        // This macro will embed the configuration file into the
+        // executable. Check include_str! for more info.
+        if let Some(config_contents) = default_config {
+            //let contents = include_str!(config_file_path);
+            settings.merge(config::File::from_str(
+                config_contents,
+                config::FileFormat::Toml,
+            ))?;
+        }
+
+        // Merge settings with env variables
+        settings.merge(Environment::with_prefix("APP"))?;
+
+        // TODO: Merge settings with Clap Settings Arguments
+
+        // Save Config to RwLoc
+        {
+            let mut w = CONFIG.write()?;
+            *w = settings;
+        }
+
+        Ok(())
+    }
+
+    pub fn merge_config(config_file: Option<&Path>) -> Result<()> {
+        // Merge settings with config file if there is one
+        if let Some(config_file_path) = config_file {
+            {
+                CONFIG
+                    .write()?
+                    .merge(config::File::with_name(config_file_path.to_str().unwrap()))?;
+            }
+        }
+        Ok(())
+    }
+
+    // Set CONFIG
+    pub fn set(key: &str, value: &str) -> Result<()> {
+        {
+            // Set Property
+            CONFIG.write()?.set(key, value)?;
+        }
+
+        Ok(())
+    }
+
+    // Get a single value
+    pub fn get<'de, T>(key: &'de str) -> Result<T>
+    where
+        T: serde::Deserialize<'de>,
+    {
+        Ok(CONFIG.read()?.get::<T>(key)?)
+    }
+
+    // Get CONFIG
+    // This clones Config (from RwLock<Config>) into a new AppConfig object.
+    // This means you have to fetch this again if you changed the configuration.
+    pub fn fetch() -> Result<AppConfig> {
+        // Get a Read Lock from RwLock
+        let r = CONFIG.read()?;
+
+        // Clone the Config object
+        let config_clone = r.deref().clone();
+
+        // Coerce Config into AppConfig
+        Ok(config_clone.try_into()?)
+    }
+}
--- pyroscope_cli/utils/src/lib.rs
+++ pyroscope_cli/utils/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod app_config;
 pub mod error;
 pub mod logger;
