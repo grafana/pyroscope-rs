539a8ee241d2f7f626897e6a23c290deaa619424
Abid Omar <contact@omarabid.com>
2022-03-06 02:02:36 +0100

    imp(backend): implement Display for StackTrace and StackFrame

--- pyroscope_backends/src/types.rs
+++ pyroscope_backends/src/types.rs
@@ -138,6 +138,21 @@ pub struct StackTrace {
     pub frames: Vec<StackFrame>,
 }
 
+impl StackTrace {
+    /// Create a new StackTrace
+    pub fn new(
+        pid: Option<u32>, thread_id: Option<u64>, thread_name: Option<String>,
+        frames: Vec<StackFrame>,
+    ) -> Self {
+        Self {
+            pid,
+            thread_id,
+            thread_name,
+            frames,
+        }
+    }
+}
+
 /// StackFrame
 #[derive(Debug, Default)]
 pub struct StackFrame {
@@ -153,6 +168,51 @@ pub struct StackFrame {
     pub absolute_path: Option<String>,
     /// Line number
     pub line: Option<u32>,
+}
+
+impl StackFrame {
+    /// Create a new StackFrame.
+    pub fn new(
+        module: Option<String>, name: Option<String>, filename: Option<String>,
+        relative_path: Option<String>, absolute_path: Option<String>, line: Option<u32>,
+    ) -> Self {
+        Self {
+            module,
+            name,
+            filename,
+            relative_path,
+            absolute_path,
+            line,
+        }
+    }
+}
+
+impl std::fmt::Display for StackFrame {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}:{} - {}",
+            self.filename.as_ref().unwrap_or(&"".to_string()),
+            self.line.unwrap_or(0),
+            self.name.as_ref().unwrap_or(&"".to_string())
+        )
+    }
+}
+
+impl std::fmt::Display for StackTrace {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            &self
+                .frames
+                .iter()
+                .rev()
+                .map(|frame| format!("{}", frame))
+                .collect::<Vec<_>>()
+                .join(";")
+        )
+    }
 }
 
 #[cfg(test)]
@@ -205,5 +265,47 @@ mod tests {
         assert_eq!(backend.get_state(), State::Running);
         assert!(backend.stop().is_ok());
         assert_eq!(backend.get_state(), State::Ready);
+    }
+
+    #[test]
+    fn test_stack_frame_display() {
+        let frame = StackFrame::new(
+            Some("module".to_string()),
+            Some("name".to_string()),
+            Some("filename".to_string()),
+            Some("relative_path".to_string()),
+            Some("absolute_path".to_string()),
+            Some(1),
+        );
+
+        assert_eq!(format!("{}", frame), "filename:1 - name");
+    }
+
+    #[test]
+    fn test_stack_trace_display() {
+        let mut frames = Vec::new();
+        frames.push(StackFrame::new(
+            Some("module".to_string()),
+            Some("name".to_string()),
+            Some("filename".to_string()),
+            Some("relative_path".to_string()),
+            Some("absolute_path".to_string()),
+            Some(1),
+        ));
+        frames.push(StackFrame::new(
+            Some("module".to_string()),
+            Some("name".to_string()),
+            Some("filename".to_string()),
+            Some("relative_path".to_string()),
+            Some("absolute_path".to_string()),
+            Some(2),
+        ));
+
+        let stack_trace = StackTrace::new(None, None, None, frames);
+
+        assert_eq!(
+            format!("{}", stack_trace),
+            "filename:2 - name;filename:1 - name"
+        );
     }
 }
