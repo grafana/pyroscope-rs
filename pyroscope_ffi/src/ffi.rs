b93c41cfce15a139ba5acd17871c2480751b78dd
Abid Omar <contact@omarabid.com>
2022-03-07 23:17:30 +0100

    wip: FFI update

--- pyroscope_ffi/src/ffi.rs
+++ pyroscope_ffi/src/ffi.rs
@@ -1,6 +1,7 @@
-use crate::PyroscopeAgent;
-use pyroscope_backends::pyspy::Pyspy;
+use pyroscope::PyroscopeAgent;
+use pyroscope_backends::pyspy::PyspyConfig;
 use pyroscope_backends::rbspy::Rbspy;
+use pyroscope_backends::{pyspy::Pyspy, rbspy::RbspyConfig};
 
 use std::sync::{Arc, Mutex};
 
@@ -16,12 +17,14 @@ pub fn add(x: i32, y: i32) -> i32 {
 #[no_mangle]
 pub fn initialize_agent(pid: i32) -> bool {
     std::thread::spawn(move || {
+        let config = RbspyConfig::new(pid, 100, false, None, true);
+
         let mut agent = PyroscopeAgent::builder("http://localhost:4040", "rbspy.basic")
-            .backend(Rbspy::new(pid))
+            .backend(Rbspy::new(config))
             .build()
             .unwrap();
 
-        agent.start();
+        agent.start().unwrap();
 
         // should instead block here until a stop signal is received
         loop {
@@ -36,13 +39,21 @@ pub fn initialize_agent(pid: i32) -> bool {
 #[no_mangle]
 pub fn initialize_python(pid: i32) -> bool {
     std::thread::spawn(move || {
+        let config = PyspyConfig::new(pid)
+            .sample_rate(100)
+            .lock_process(true)
+            .time_limit(None)
+            .with_subprocesses(true)
+            .include_idle(false)
+            .native(false);
+
         let mut agent = PyroscopeAgent::builder("http://localhost:4040", "python.basic")
-            .backend(Pyspy::new(pid))
+            .backend(Pyspy::new(config))
             .build()
             .unwrap();
         std::thread::sleep(std::time::Duration::from_millis(5000));
 
-        agent.start();
+        agent.start().unwrap();
 
         // should instead block here until a stop signal is received
         loop {
