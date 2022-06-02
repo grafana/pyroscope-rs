3f76ab261cefe0fe9c5c314f6f0b14622f175af9
Abid Omar <contact@omarabid.com>
2022-06-02 00:14:39 -0500

    wip: checkpoint

--- pyroscope_ffi/ruby/ext/thread_id/extconf.rb
+++ pyroscope_ffi/ruby/ext/thread_id/extconf.rb
@@ -1,5 +1,11 @@
 require 'mkmf'
-require 'rb_sys/mkmf'
-#require 'rake'
+require 'rake'
 
-create_rust_makefile('thread_id')
+create_makefile('thread_id')
+
+app = Rake.application
+app.init
+app.add_import 'Rakefile'
+app.load_rakefile
+
+app['default'].invoke
