98d224629617af14041700aee65f7ac0a70a7314
Abid Omar <contact@omarabid.com>
2022-06-18 00:26:42 -0500

    wip: python wheels

---
+++ pyroscope_ffi/ruby/elflib/thread_id/setup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from setuptools import find_packages, setup
+from pathlib import Path
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+
+os.chdir(SCRIPT_DIR)
+
+# print script_dir
+print(SCRIPT_DIR)
+
+LIB_DIR = str(SCRIPT_DIR / "../../ext/thread_id")
+
+def build_native(spec):
+    # Step 1: build the rust library
+    build = spec.add_external_build(
+        cmd=['cargo', 'build', '--release'],
+        path=LIB_DIR
+    )
+
+    def find_dylib():
+        cargo_target = os.environ.get('CARGO_BUILD_TARGET')
+        if cargo_target:
+            in_path = 'target/%s/release' % (cargo_target)
+        else:
+            in_path = 'target/release'
+        return build.find_dylib('thread_id', in_path=in_path)
+
+    # Step 2: package the compiled library
+    rtld_flags = ["NOW"]
+    if sys.platform == "darwin":
+        rtld_flags.append("NODELETE")
+
+    spec.add_cffi_module(module_path='thread_id._native',
+            dylib=find_dylib,
+            header_filename=lambda:
+            build.find_header('thread_id.h',in_path='include'),
+            rtld_flags=rtld_flags,
+    )
+
+setup(
+    platforms="any",
+    milksnake_tasks=[build_native],
+    setup_requires=["milksnakex>=0.1.6"],
+)
