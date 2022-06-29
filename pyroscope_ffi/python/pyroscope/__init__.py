7da1df8ac1e8308f0953520f5267abd295daad8a
Abid Omar <contact@omarabid.com>
2022-06-29 12:06:34 -0500

    imp(pyspy): add deprecation warnings

--- pyroscope_ffi/python/pyroscope/__init__.py
+++ pyroscope_ffi/python/pyroscope/__init__.py
@@ -1,14 +1,19 @@
 import threading
+import warnings
 from collections import namedtuple
 from pyroscope._native import ffi, lib
 from contextlib import contextmanager
 
 Config = namedtuple('Config', ('application_name', 'server_address', 'auth_token', 'sample_rate', 'detect_subprocesses','oncpu', 'native', 'gil_only', 'log_level'))
 
-def configure(application_name=None, server_address="http://localhost:4040", auth_token = "", sample_rate=100, detect_subprocesses=False, oncpu=False, native=False, gil_only=False, log_level="info", tags=None): 
+def configure(application_name=None, server_address="http://localhost:4040",
+        auth_token = "", sample_rate=100, detect_subprocesses=False,
+        oncpu=False, native=False, gil_only=False, report_pid=False, report_thread_id=False,
+        report_thread_name=False, log_level="info", tags=None): 
     lib.initialize_agent(application_name.encode("UTF-8"),
             server_address.encode("UTF-8"), auth_token.encode("UTF-8"), sample_rate, detect_subprocesses,
-            oncpu, native, gil_only,
+            oncpu, native, gil_only, report_pid, report_thread_id,
+            report_thread_name,
             tags_to_string(tags).encode("UTF-8"))
 
 def add_thread_tag(thread_id, key, value):
@@ -38,3 +43,16 @@ def tag_wrapper(tags):
     finally:
         for key, value in tags.items():
             lib.remove_thread_tag(threading.get_ident(), key.encode("UTF-8"), value.encode("UTF-8"))
+
+def stop():
+    warnings.warn("deprecated, no longer applicable", DeprecationWarning)
+def change_name(name):
+    warnings.warn("deprecated, no longer applicable", DeprecationWarning)
+def tag(tags):
+    warnings.warn("deprecated, use tag_wrapper function", DeprecationWarning)
+def remove_tags(*keys):
+    warnings.warn("deprecated, no longer applicable", DeprecationWarning)
+def build_summary():
+    warnings.warn("deprecated, no longer applicable", DeprecationWarning)
+def test_logger():
+    warnings.warn("deprecated, no longer applicable", DeprecationWarning)
