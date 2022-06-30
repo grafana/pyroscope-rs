0cff44a206303e43cf952d334e8c156fb2f32ba3
Abid Omar <contact@omarabid.com>
2022-06-30 13:43:24 -0500

    chore(ops): remove commented lines

--- pyroscope_ffi/ruby/elflib/thread_id/manylinux.sh
+++ pyroscope_ffi/ruby/elflib/thread_id/manylinux.sh
@@ -2,20 +2,20 @@
 set -e
 
 # Install tooling
-#yum -y -q install wget gcc libffi-devel openssl-devel
+yum -y -q install wget gcc libffi-devel openssl-devel
 
 # Install Rust
-#curl https://sh.rustup.rs -sSf | sh -s -- -y
-#export PATH=~/.cargo/bin:$PATH
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+export PATH=~/.cargo/bin:$PATH
 
 # Build wheels
-#/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
+/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
 
 # Audit wheels
-#for wheel in dist/*.whl; do
-  #auditwheel repair $wheel -w dist/
-  #rm $wheel
-#done
+for wheel in dist/*.whl; do
+  auditwheel repair $wheel -w dist/
+  rm $wheel
+done
 
 # Extract wheels
 for wheel in dist/*.whl; do
