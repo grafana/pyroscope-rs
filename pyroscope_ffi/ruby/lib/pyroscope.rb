630c37e8dfcd04c11de7bb65c29532b25f94eefd
Dmitry Filimonov <dmitry@pyroscope.io>
2022-07-05 00:59:23 -0700

    fix: namespaces modules to prevent namespace collisions

--- pyroscope_ffi/ruby/lib/pyroscope.rb
+++ pyroscope_ffi/ruby/lib/pyroscope.rb
@@ -1,21 +1,21 @@
 require 'ffi'
 
-module Rust
-  extend FFI::Library
-  ffi_lib File.expand_path(File.dirname(__FILE__)) + "/rbspy/rbspy.#{RbConfig::CONFIG["DLEXT"]}"
-  attach_function :initialize_agent, [:string, :string, :string, :int, :bool, :bool, :bool, :bool, :string], :bool
-  attach_function :add_tag, [:uint64, :string, :string], :bool
-  attach_function :remove_tag, [:uint64, :string, :string], :bool
-  attach_function :drop_agent, [], :bool
-end
-
-module Utils
-  extend FFI::Library
-  ffi_lib File.expand_path(File.dirname(__FILE__)) + "/thread_id/thread_id.#{RbConfig::CONFIG["DLEXT"]}"
-  attach_function :thread_id, [], :uint64
-end
-
 module Pyroscope
+  module Rust
+    extend FFI::Library
+    ffi_lib File.expand_path(File.dirname(__FILE__)) + "/rbspy/rbspy.#{RbConfig::CONFIG["DLEXT"]}"
+    attach_function :initialize_agent, [:string, :string, :string, :int, :bool, :bool, :bool, :bool, :string], :bool
+    attach_function :add_tag, [:uint64, :string, :string], :bool
+    attach_function :remove_tag, [:uint64, :string, :string], :bool
+    attach_function :drop_agent, [], :bool
+  end
+
+  module Utils
+    extend FFI::Library
+    ffi_lib File.expand_path(File.dirname(__FILE__)) + "/thread_id/thread_id.#{RbConfig::CONFIG["DLEXT"]}"
+    attach_function :thread_id, [], :uint64
+  end
+
   Config = Struct.new(:application_name, :app_name, :server_address, :auth_token, :sample_rate, :detect_subprocesses, :on_cpu, :report_pid, :report_thread_id, :log_level, :tags) do
     def initialize(*)
       self.application_name = ''
