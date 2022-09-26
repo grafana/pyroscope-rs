3993b38a4e0a2e689c986782d85bf7836aafb3b0
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-09-26 14:09:50 +0700

    fix rust version

--- pyroscope_ffi/ruby/scripts/docker.sh
+++ pyroscope_ffi/ruby/scripts/docker.sh
@@ -3,13 +3,13 @@ set -ex
 
 BUILD_DIR="/work"
 
-docker run -ti \
+docker run \
         -w /work/pyroscope_ffi/ruby/elflib/rbspy \
         -v `pwd`:/work \
         quay.io/pypa/${BUILD_ARCH} \
         sh manylinux.sh
 
-docker run -ti \
+docker run \
         -w /work/pyroscope_ffi/ruby/elflib/thread_id \
         -v `pwd`:/work \
         quay.io/pypa/${BUILD_ARCH} \
