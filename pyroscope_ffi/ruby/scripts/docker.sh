6bd45eedc505ec0e6132c7b6b1d71f20e59d678f
Tolyan Korniltsev <korniltsev.anatoly@gmail.com>
2023-02-18 16:26:01 +0700

    fix

--- pyroscope_ffi/ruby/scripts/docker.sh
+++ pyroscope_ffi/ruby/scripts/docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+if [ "${BUILD_ARCH}" != "manylinux2010_x86_64" ] && [ "${BUILD_ARCH}" != "manylinux2014_aarch64" ];
+then
+  echo set BUILD_ARCH to one of manylinux2010_x86_64 or manylinux2014_aarch64
+  exit 239
+fi
 BUILD_DIR="/work"
 MANYLINUX_PREFIX=pyroscope/rust_builder
 docker run --rm -ti \
