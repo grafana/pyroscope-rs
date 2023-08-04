0d83703309c0cbd42d31571fb0a3f255978d3913
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-08-04 19:48:12 +0700

    fix

--- pyroscope_ffi/ruby/scripts/docker.sh
+++ pyroscope_ffi/ruby/scripts/docker.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -ex
 
-if [ "${BUILD_ARCH}" != "manylinux2010_x86_64" ] && [ "${BUILD_ARCH}" != "manylinux2014_aarch64" ];
+if [ "${BUILD_ARCH}" != "manylinux2014_x86_64" ] && [ "${BUILD_ARCH}" != "manylinux2014_aarch64" ];
 then
-  echo set BUILD_ARCH to one of manylinux2010_x86_64 or manylinux2014_aarch64
+  echo set BUILD_ARCH to one of manylinux2014_x86_64 or manylinux2014_aarch64
   exit 239
 fi
 
 BUILD_DIR="/work"
 MANYLINUX_PREFIX=pyroscope/rust_builder
+MANYLINUX_VERSION=2
 
 docker run \
         -w /work/pyroscope_ffi/ruby/elflib/rbspy \
         -v `pwd`:/work \
-        ${MANYLINUX_PREFIX}_${BUILD_ARCH} \
+        ${MANYLINUX_PREFIX}_${BUILD_ARCH}:${MANYLINUX_VERSION} \
         sh manylinux.sh
 
 docker run \
         -w /work/pyroscope_ffi/ruby/elflib/thread_id \
         -v `pwd`:/work \
-        ${MANYLINUX_PREFIX}_${BUILD_ARCH} \
+        ${MANYLINUX_PREFIX}_${BUILD_ARCH}:${MANYLINUX_VERSION} \
         sh manylinux.sh
