12b96e83cb312e5bb3bf7b1f2d24ba9cf209e408
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-04-18 17:16:59 +0700

    fix: pyroscope-cli compilation
    - fix compilation
    - remove config file merging feature
    - remove slog dependencies
    
    Signed-off-by: Tolya Korniltsev <korniltsev.anatoly@gmail.com>

--- pyroscope_cli/src/main.rs
+++ pyroscope_cli/src/main.rs
@@ -1,42 +1,22 @@
-#[cfg(not(debug_assertions))]
-use human_panic::setup_panic;
-
-#[cfg(debug_assertions)]
-extern crate better_panic;
-
+use clap::Parser;
 pub mod cli;
 pub mod core;
 pub mod utils;
 
-use crate::utils::{app_config::AppConfig, error::Result};
+use crate::core::commands;
+use crate::utils::error::Result;
 
-/// The main entry for the application.
 fn main() -> Result<()> {
-    // Human Panic. Only enabled when *not* debugging.
-    #[cfg(not(debug_assertions))]
-    {
-        setup_panic!();
+    let cli1 = cli::Cli::parse();
+    match cli1.command {
+        cli::Commands::Exec(exec) => {
+            let _guard = utils::logger::setup_logging(exec.common.log_level)?;
+            commands::exec(exec.command, exec.common)?;
+        }
+        cli::Commands::Connect(connect) => {
+            let _guard = utils::logger::setup_logging(connect.common.log_level)?;
+            commands::connect(connect.pid, connect.common)?;
+        }
     }
-
-    // Better Panic. Only enabled *when* debugging.
-    #[cfg(debug_assertions)]
-    {
-        better_panic::Settings::debug()
-            .most_recent_first(false)
-            .lineno_suffix(true)
-            .verbosity(better_panic::Verbosity::Full)
-            .install();
-    }
-
-    // Setup logging
-    let _guard = utils::logger::setup_logging()?;
-
-    // Initialize Configuration
-    let config_contents = include_str!("resources/default_config.toml");
-    AppConfig::init(Some(config_contents))?;
-
-    // Match Commands
-    crate::cli::lib::cli_match()?;
-
     Ok(())
 }
