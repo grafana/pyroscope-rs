926a4ba53b38ae9dd8ad6d2cc732a491941cf69b
korniltsev-grafanista <anatoly.korniltsev@grafana.com>
2026-02-11 10:58:37 +0700

    ruby: remove detect_subprocess (#300)

--- pyroscope_ffi/ruby/lib/pyroscope.rb
+++ pyroscope_ffi/ruby/lib/pyroscope.rb
@@ -9,7 +9,7 @@ module Pyroscope
     extend FFI::Library
     ffi_lib File.expand_path(File.dirname(__FILE__)) + "/rbspy/rbspy.#{RbConfig::CONFIG["DLEXT"]}"
     attach_function :initialize_logging, [:int], :bool
-    attach_function :initialize_agent, [:string, :string, :string, :string, :string, :int, :bool, :bool, :bool, :bool, :string, :string, :string, :string, :string], :bool
+    attach_function :initialize_agent, [:string, :string, :string, :string, :string, :int, :bool, :bool, :bool, :string, :string, :string, :string, :string], :bool
     attach_function :add_thread_tag, [:uint64, :string, :string], :bool
     attach_function :remove_thread_tag, [:uint64, :string, :string], :bool
     attach_function :add_global_tag, [:string, :string], :bool
@@ -113,7 +113,6 @@ module Pyroscope
         @config.basic_auth_username || "",
         @config.basic_auth_password || "",
         @config.sample_rate || 100,
-        @config.detect_subprocesses || false,
         @config.oncpu || false,
         @config.report_pid || false,
         @config.report_thread_id || false,
