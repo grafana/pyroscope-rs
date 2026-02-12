358cf0601ca32fe404327acacf707d4f82c85ccf
korniltsev-grafanista <anatoly.korniltsev@grafana.com>
2026-02-13 00:33:23 +0700

    python ffi: replace milksnake with setuptools-rust + cffi

--- pyroscope_ffi/python/setup.py
+++ pyroscope_ffi/python/setup.py
@@ -1,41 +1,21 @@
-import os
-import sys
-from setuptools import find_packages, setup
 from pathlib import Path
 
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
 SCRIPT_DIR = Path(__file__).resolve().parent
-os.chdir(SCRIPT_DIR)
-LIB_DIR = str(SCRIPT_DIR / "lib")
-
-def build_native(spec):
-    # Step 1: build the rust library
-    build = spec.add_external_build(
-        cmd=['cargo', 'build', '--package', 'pyroscope_ffi', '--profile', 'release'],
-        path=LIB_DIR
-    )
-
-    def find_dylib():
-        cargo_target = os.environ.get('CARGO_BUILD_TARGET')
-        if cargo_target:
-            in_path = '../../../target/%s/release' % (cargo_target)
-        else:
-            in_path = '../../../target/release'
-        return build.find_dylib('pyroscope_ffi', in_path=in_path)
-
-    # Step 2: package the compiled library
-    rtld_flags = ["NOW"]
-    if sys.platform == "darwin":
-        rtld_flags.append("NODELETE")
-
-    spec.add_cffi_module(module_path='pyroscope._native',
-            dylib=find_dylib,
-            header_filename=lambda:
-            build.find_header('pyroscope_ffi.h',in_path='include'),
-            rtld_flags=rtld_flags,
-    )
 
 setup(
     platforms="any",
-    milksnake_tasks=[build_native],
-    setup_requires=["pyromilksnakex==0.1.8", "cffi>=1.6.0", "pycparser"],
+    rust_extensions=[
+        RustExtension(
+            "pyroscope._native_lib",
+            path=str(SCRIPT_DIR / "lib" / "Cargo.toml"),
+            binding=Binding.NoBinding,
+            debug=False,
+        )
+    ],
+    setup_requires=["setuptools-rust>=1.8.0", "cffi>=1.6.0", "pycparser"],
+    include_package_data=True,
+    zip_safe=False,
 )
