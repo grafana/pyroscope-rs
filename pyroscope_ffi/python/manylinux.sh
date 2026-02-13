e21b5197f60c2e4f3fc1359b729e48913f06cea9
korniltsev-grafanista <anatoly.korniltsev@grafana.com>
2026-02-13 10:06:59 +0700

    python packaging: drop requirements.txt and use pyproject build flows

--- pyroscope_ffi/python/manylinux.sh
+++ pyroscope_ffi/python/manylinux.sh
@@ -4,9 +4,8 @@ set -ex
 cargo --version
 
 # Build wheels
-# todo this one is deprecated, use "build" package
-/opt/python/cp39-cp39/bin/python -m pip install -r requirements.txt setuptools-rust cffi pycparser
-/opt/python/cp39-cp39/bin/python setup.py bdist_wheel
+/opt/python/cp39-cp39/bin/python -m pip install --upgrade build
+/opt/python/cp39-cp39/bin/python -m build --wheel
 
 # Audit wheels
 for wheel in dist/*-linux_*.whl; do
