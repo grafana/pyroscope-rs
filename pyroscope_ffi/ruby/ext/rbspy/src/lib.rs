7cc3acf1a9d943576ce99e32e0ef102d869c9909
korniltsev-grafanista <anatoly.korniltsev@grafana.com>
2026-02-15 22:30:21 +0700

    Update rbspy version source to Ruby FFI Cargo.toml

--- pyroscope_ffi/ruby/ext/rbspy/src/lib.rs
+++ pyroscope_ffi/ruby/ext/rbspy/src/lib.rs
@@ -13,7 +13,7 @@ use pyroscope::pyroscope::PyroscopeAgentBuilder;
 
 const LOG_TAG: &str = "Pyroscope::rbspy::ffi";
 const RBSPY_NAME: &str = "rbspy";
-const RBSPY_VERSION: &str = "0.6.9";
+const RBSPY_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub fn transform_report(report: Report) -> Report {
     let cwd = env::current_dir().unwrap();
