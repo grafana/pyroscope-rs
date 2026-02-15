6a971f01ead64079c096edd7468b9c3f90683ddd
Tolya Korniltsev <anatoly.korniltsev@grafana.com>
2026-02-15 22:25:29 +0700

    fix

--- pyroscope_ffi/python/scripts/tests/compile_ffi.py
+++ pyroscope_ffi/python/scripts/tests/compile_ffi.py
@@ -1,3 +1,4 @@
+import os
 import re
 import cffi
 from cffi import recompiler as cffi_recompiler
@@ -5,8 +6,11 @@ from cffi import recompiler as cffi_recompiler
 
 _directive_re = re.compile(r'(?m)^\s*#.*?$')
 
-src = '/pyroscope_ffi/python/rust/include/pyroscope_ffi.h'
-dst = '/python/python/pyroscope/_cffi.py'
+_script_dir = os.path.dirname(os.path.abspath(__file__))
+_python_ffi_dir = os.path.normpath(os.path.join(_script_dir, '..', '..'))
+
+src = os.path.join(_python_ffi_dir, 'rust', 'include', 'pyroscope_ffi.h')
+dst = os.path.join(_python_ffi_dir, 'python', 'pyroscope', '_cffi.py')
 src = open(src, 'r').read()
 src = _directive_re.sub('', src)
 
