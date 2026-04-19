12b6d978f49093a1032f6078b7a0e9ce22fa456a
Tolya Korniltsev <anatoly.korniltsev@grafana.com>
2026-04-19 20:17:38 +1000

    tmp

--- src/backend/pprof.rs
+++ src/backend/pprof.rs
@@ -10,6 +10,12 @@ use std::{
     ops::Deref,
     sync::{Arc, Mutex},
 };
+use blazesym::symbolize::source::Process;
+use blazesym::symbolize::source::Source;
+use blazesym::symbolize::Input;
+use blazesym::symbolize::{Symbolizer, Builder};
+use blazesym::Addr;
+use blazesym::Pid;
 
 const LOG_TAG: &str = "Pyroscope::Pprofrs";
 
@@ -31,13 +37,16 @@ impl Default for PprofConfig {
     }
 }
 
-#[derive(Default)]
+
 pub struct Pprof<'a> {
     buffer: Arc<Mutex<StackBuffer>>,
     config: PprofConfig,
     backend_config: BackendConfig,
     inner_builder: Arc<Mutex<Option<ProfilerGuardBuilder>>>,
     guard: Arc<Mutex<Option<ProfilerGuard<'a>>>>,
+
+    symbolizer: Symbolizer,
+
 }
 
 impl std::fmt::Debug for Pprof<'_> {
@@ -48,12 +57,15 @@ impl std::fmt::Debug for Pprof<'_> {
 
 impl<'a> Pprof<'a> {
     pub fn new(config: PprofConfig, backend_config: BackendConfig) -> Self {
+        let symbolizer = Symbolizer::builder()
+            .build();
         Pprof {
             buffer: Arc::new(Mutex::new(StackBuffer::default())),
             config,
             backend_config,
             inner_builder: Arc::new(Mutex::new(None)),
             guard: Arc::new(Mutex::new(None)),
+            symbolizer
         }
     }
 }
@@ -117,111 +129,111 @@ impl Pprof<'_> {
             .as_ref()
             .ok_or_else(|| PyroscopeError::new("pprof-rs: ProfilerGuard report error"))?
             .report()
-            .build_and_clear(true)
+            .build_unresolved_and_reset()
             .map_err(|e| PyroscopeError::new(e.to_string().as_str()))?;
 
-        let stack_buffer = Into::<StackBuffer>::into(Into::<StackBufferWrapper>::into((
-            report,
-            &self.backend_config,
-        )));
-
-        {
-            let mut buffer = self.buffer.lock()?;
-            for (stacktrace, count) in stack_buffer.data {
-                buffer.record_with_count(stacktrace, count)?;
-            }
-        }
+        // let stack_buffer = Into::<StackBuffer>::into(Into::<StackBufferWrapper>::into((
+        //     report,
+        //     &self.backend_config,
+        // )));
+        //
+        // {
+        //     let mut buffer = self.buffer.lock()?;
+        //     for (stacktrace, count) in stack_buffer.data {
+        //         buffer.record_with_count(stacktrace, count)?;
+        //     }
+        // }
 
         Ok(())
     }
 }
-
-struct StackBufferWrapper(StackBuffer);
-
-impl From<StackBufferWrapper> for StackBuffer {
-    fn from(stackbuffer: StackBufferWrapper) -> Self {
-        stackbuffer.0
-    }
-}
-
-impl From<(pprof::Report, &BackendConfig)> for StackBufferWrapper {
-    fn from(arg: (pprof::Report, &BackendConfig)) -> Self {
-        let (report, config) = arg;
-        let buffer_data: HashMap<StackTrace, usize> = report
-            .data
-            .iter()
-            .map(|(key, value)| {
-                (
-                    Into::<StackTraceWrapper>::into((key.to_owned(), config)).into(),
-                    value.to_owned() as usize,
-                )
-            })
-            .collect();
-        StackBufferWrapper(StackBuffer::new(buffer_data))
-    }
-}
-
-struct StackTraceWrapper(StackTrace);
-
-impl From<StackTraceWrapper> for StackTrace {
-    fn from(stack_trace: StackTraceWrapper) -> Self {
-        stack_trace.0
-    }
-}
-
-impl From<(pprof::Frames, &BackendConfig)> for StackTraceWrapper {
-    fn from(arg: (pprof::Frames, &BackendConfig)) -> Self {
-        let (frames, config) = arg;
-        let thread_id = frames.thread_id as libc::pthread_t;
-        StackTraceWrapper(StackTrace::new(
-            config,
-            None,
-            Some(thread_id.into()),
-            Some(frames.thread_name),
-            frames
-                .frames
-                .concat()
-                .iter()
-                .map(|frame| Into::<StackFrameWrapper>::into(frame.to_owned()).into())
-                .collect(),
-        ))
-    }
-}
-
-struct StackFrameWrapper(StackFrame);
-
-impl From<StackFrameWrapper> for StackFrame {
-    fn from(stack_frame: StackFrameWrapper) -> Self {
-        stack_frame.0
-    }
-}
-
-impl From<pprof::Symbol> for StackFrameWrapper {
-    fn from(symbol: pprof::Symbol) -> Self {
-        StackFrameWrapper(StackFrame::new(
-            None,
-            Some(symbol.name()),
-            Some(
-                symbol
-                    .filename
-                    .clone()
-                    .unwrap_or_default()
-                    .file_name()
-                    .unwrap_or_else(|| OsStr::new(""))
-                    .to_str()
-                    .unwrap_or("")
-                    .to_string(),
-            ),
-            Some(
-                symbol
-                    .filename
-                    .unwrap_or_default()
-                    .to_str()
-                    .unwrap_or("")
-                    .to_string(),
-            ),
-            None,
-            symbol.lineno,
-        ))
-    }
-}
+//
+// struct StackBufferWrapper(StackBuffer);
+//
+// impl From<StackBufferWrapper> for StackBuffer {
+//     fn from(stackbuffer: StackBufferWrapper) -> Self {
+//         stackbuffer.0
+//     }
+// }
+//
+// impl From<(pprof::Report, &BackendConfig)> for StackBufferWrapper {
+//     fn from(arg: (pprof::Report, &BackendConfig)) -> Self {
+//         let (report, config) = arg;
+//         let buffer_data: HashMap<StackTrace, usize> = report
+//             .data
+//             .iter()
+//             .map(|(key, value)| {
+//                 (
+//                     Into::<StackTraceWrapper>::into((key.to_owned(), config)).into(),
+//                     value.to_owned() as usize,
+//                 )
+//             })
+//             .collect();
+//         StackBufferWrapper(StackBuffer::new(buffer_data))
+//     }
+// }
+//
+// struct StackTraceWrapper(StackTrace);
+//
+// impl From<StackTraceWrapper> for StackTrace {
+//     fn from(stack_trace: StackTraceWrapper) -> Self {
+//         stack_trace.0
+//     }
+// }
+//
+// impl From<(pprof::Frames, &BackendConfig)> for StackTraceWrapper {
+//     fn from(arg: (pprof::Frames, &BackendConfig)) -> Self {
+//         let (frames, config) = arg;
+//         let thread_id = frames.thread_id as libc::pthread_t;
+//         StackTraceWrapper(StackTrace::new(
+//             config,
+//             None,
+//             Some(thread_id.into()),
+//             Some(frames.thread_name),
+//             frames
+//                 .frames
+//                 .concat()
+//                 .iter()
+//                 .map(|frame| Into::<StackFrameWrapper>::into(frame.to_owned()).into())
+//                 .collect(),
+//         ))
+//     }
+// }
+//
+// struct StackFrameWrapper(StackFrame);
+//
+// impl From<StackFrameWrapper> for StackFrame {
+//     fn from(stack_frame: StackFrameWrapper) -> Self {
+//         stack_frame.0
+//     }
+// }
+//
+// impl From<pprof::Symbol> for StackFrameWrapper {
+//     fn from(symbol: pprof::Symbol) -> Self {
+//         StackFrameWrapper(StackFrame::new(
+//             None,
+//             Some(symbol.name()),
+//             Some(
+//                 symbol
+//                     .filename
+//                     .clone()
+//                     .unwrap_or_default()
+//                     .file_name()
+//                     .unwrap_or_else(|| OsStr::new(""))
+//                     .to_str()
+//                     .unwrap_or("")
+//                     .to_string(),
+//             ),
+//             Some(
+//                 symbol
+//                     .filename
+//                     .unwrap_or_default()
+//                     .to_str()
+//                     .unwrap_or("")
+//                     .to_string(),
+//             ),
+//             None,
+//             symbol.lineno,
+//         ))
+//     }
+// }
