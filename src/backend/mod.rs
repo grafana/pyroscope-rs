6dee942f96bbf274b6d7376c4b31da588da72f95
Tolyan Korniltsev <anatoly.korniltsev@grafana.com>
2026-05-28 17:13:02 +0700

    Update src/backend/mod.rs
    Co-authored-by: Christian Simon <simon@swine.de>

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,10 +1,26 @@
 pub mod backend;
 #[cfg(feature = "backend-jemalloc")]
 pub mod jemalloc;
-#[cfg(feature = "backend-pprof-rs")]
+#[cfg(all(
+    feature = "backend-pprof-rs",
+    any(
+        all(target_os = "linux", any(target_arch = "x86_64", target_arch = "aarch64")),
+        all(target_os = "macos", any(target_arch = "x86_64", target_arch = "aarch64"))
+    )
+))]
 pub mod pprof;
-#[cfg(feature = "backend-pprof-rs")]
-mod pprofrs;
+
+[...]
+
+#[cfg(all(
+    feature = "backend-pprof-rs",
+    not(any(
+        all(target_os = "linux", any(target_arch = "x86_64", target_arch = "aarch64")),
+        all(target_os = "macos", any(target_arch = "x86_64", target_arch = "aarch64"))
+    ))
+))]
+compile_error!("feature `backend-pprof-rs` is only supported on Linux/macOS x86_64/aarch64");
+
 pub mod ruleset;
 pub mod tests;
 pub mod types;
